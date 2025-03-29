f1ffdc707cf0d4fc0db0606a8e66975ad67ee138
August <vuonglocthinh2003@gmail.com>
2025-03-29 14:26:29 +0700

    Edit Message.ts

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -7,6 +7,7 @@ export default class Message {
   id: number;
   userChatbotId: number;
   tenantId: number;
+  content: string;
   sender: Sender;
   createAt: Date;
 
@@ -14,11 +15,13 @@ export default class Message {
     id: number,
     userChatbotId: number,
     tenantId: number,
+    content: string,
     sender: Sender,
   ) {
     this.id = id;
     this.userChatbotId = userChatbotId;
     this.tenantId = tenantId;
+    this.content = content;
     this.sender = sender;
     this.createAt = new Date();
   }
