e43c5dc6544e4dba03e30ec691563a2861774c62
thienan137 <92162400+thienan137@users.noreply.github.com>
2025-04-05 02:30:50 +0700

    Add SubscriptionPlan service

---
+++ src/services/SubscriptionPlanService.ts
@@ -0,0 +1,23 @@
+import SubscriptionPlan from "../models/SubscriptionPlan";
+
+export class SubscriptionPlanService {
+  async getAllPlans() {
+    return await SubscriptionPlan.findAll();
+  }
+
+  async getPlanById(id: number) {
+    return await SubscriptionPlan.findByPk(id);
+  }
+
+  async createPlan(planData: any) {
+    return await SubscriptionPlan.create(planData);
+  }
+
+  async updatePlan(id: number, planData: any) {
+    return await SubscriptionPlan.update(planData, { where: { id } });
+  }
+
+  async deletePlan(id: number) {
+    return await SubscriptionPlan.destroy({ where: { id } });
+  }
+}
