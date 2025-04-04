ca0a132d52aa8582168b72bca3914220203505bc
thienan137 <92162400+thienan137@users.noreply.github.com>
2025-04-05 02:32:54 +0700

    Add subscription service

---
+++ src/services/SubscriptionService.ts
@@ -0,0 +1,23 @@
+import Subscription from "../models/Subscription";
+
+export class SubscriptionService {
+  async getAllSubscriptions() {
+    return await Subscription.findAll();
+  }
+
+  async getSubscriptionById(id: number) {
+    return await Subscription.findByPk(id);
+  }
+
+  async createSubscription(subscriptionData: any) {
+    return await Subscription.create(subscriptionData);
+  }
+
+  async updateSubscription(id: number, subscriptionData: any) {
+    return await Subscription.update(subscriptionData, { where: { id } });
+  }
+
+  async deleteSubscription(id: number) {
+    return await Subscription.destroy({ where: { id } });
+  }
+}
