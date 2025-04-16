66c06c42f04fb4efe949398447c442f5008f5d8a
August <vuonglocthinh2003@gmail.com>
2025-04-16 20:58:40 +0700

    Add properties to feddback dto

--- src/dtos/FeedbackDto.ts
+++ src/dtos/FeedbackDto.ts
@@ -1,1 +1,25 @@
+import { ChatSession } from '../models/ChatSession';
 
+enum Rating {
+  Positive = 1,
+  Negative = 0,
+}
+
+export default class Feedback {
+  id: number;
+  chatSession: ChatSession;
+  rating: Rating;
+  comment: string;
+
+  constructor(
+    id: number,
+    chatSession: ChatSession,
+    rating: Rating,
+    comment: string,
+  ) {
+    this.id = id;
+    this.chatSession = chatSession;
+    this.rating = rating;
+    this.comment = comment;
+  }
+}
