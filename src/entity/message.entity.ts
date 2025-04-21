66d5307e75051a46014ce6f7c259651edc95d9ff
August <vuonglocthinh2003@gmail.com>
2025-04-21 21:57:02 +0700

    Add message entity and Edit chat session entity

---
+++ src/entity/message.entity.ts
@@ -0,0 +1,43 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Sender } from '../constants/SenderEnum';
+import { ChatSessionEntity } from './chatSession.entity';
+
+@Entity({
+  name: 'message',
+})
+export class MessageEntity extends BaseEntity {
+  @PrimaryGeneratedColumn({
+    name: 'id',
+  })
+  id: number;
+
+  @Column({
+    name: 'sender',
+    type: 'enum',
+    enum: Sender,
+  })
+  sender: Sender;
+
+  @Column({
+    name: 'content',
+  })
+  content: string;
+
+  @Column({
+    name: 'chat_session_id',
+  })
+  chatSessionId: number;
+
+  @ManyToOne(() => ChatSessionEntity, (chatSession) => chatSession.messages)
+  @JoinColumn({
+    name: 'chat_session_id',
+  })
+  chatSession: ChatSessionEntity;
+}
