fb9ec9a49cbb3ccbdd8b9cbc0864528217c25260
August <vuonglocthinh2003@gmail.com>
2025-04-23 15:44:37 +0700

    Change return type for save function in Feedback repository

--- src/repository/feedback.repository.ts
+++ src/repository/feedback.repository.ts
@@ -6,16 +6,7 @@ export class FeedbackRepository {
   private readonly feedbackRepository =
     myDataSource.getRepository(FeedbackEntity);
 
-  public async save(feedback: FeedbackEntity): Promise<FeedbackDto> {
-    const feedbackDto = new FeedbackDto();
-
-    feedbackDto.id = feedback.id;
-    feedbackDto.rating = feedback.rating;
-    feedbackDto.comment = feedback.comment;
-    feedbackDto.messageId = feedback.messageId;
-    feedbackDto.createdAt =
-      feedback.createdAt.toTimeString() + feedback.createdAt.toDateString();
-
-    return feedbackDto;
+  public async save(feedback: FeedbackEntity): Promise<FeedbackEntity> {
+    return await this.feedbackRepository.save(feedback);
   }
 }
