bccca71d1e926c2c4f5e6be8bf2588e77b09b6e2
August <vuonglocthinh2003@gmail.com>
2025-04-23 16:31:06 +0700

    Create Feedback controller and add post api

--- src/controller/FeedbackController.ts
+++ src/controller/FeedbackController.ts
@@ -1,62 +1,80 @@
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 import { FeedbackService } from '../services/FeedbackService';
+import { CreateFeedbackDto } from '../dto/createFeedback.dto';
+import { Rating } from '../constants/RatingEnum';
 
+const router = Router();
 const feedbackService = new FeedbackService();
 
-export const getFeedbacks = async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   try {
-    const feedbacks = await feedbackService.getAllFeedbacks();
-    res.json(feedbacks);
+    const createFeedbackDto = new CreateFeedbackDto();
+
+    createFeedbackDto.rating = req.body.rating as Rating;
+    createFeedbackDto.comment = req.body.comment;
+    createFeedbackDto.messageId = req.body.messageId;
+
+    const feedbackDto = await feedbackService.create(createFeedbackDto);
+    res.status(201).json(feedbackDto);
   } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch feedbacks', error });
+    res.status(500).json({ message: 'Failed to create feedback', error });
   }
-};
+});
 
-export const getFeedback = async (req: Request, res: Response) => {
-  try {
-    const id = parseInt(req.params.id);
-    const feedback = await feedbackService.getFeedbackById(id);
-    if (!feedback) {
-      return res.status(404).json({ message: 'Feedback not found' });
-    }
-    res.json(feedback);
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch feedback', error });
-  }
-};
+// export const getFeedbacks = async (req: Request, res: Response) => {
+//   try {
+//     const feedbacks = await feedbackService.getAllFeedbacks();
+//     res.json(feedbacks);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to fetch feedbacks', error });
+//   }
+// };
 
-export const createFeedback = async (req: Request, res: Response) => {
-  try {
-    const { chatSessionId, rating, content } = req.body;
+// export const getFeedback = async (req: Request, res: Response) => {
+//   try {
+//     const id = parseInt(req.params.id);
+//     const feedback = await feedbackService.getFeedbackById(id);
+//     if (!feedback) {
+//       return res.status(404).json({ message: 'Feedback not found' });
+//     }
+//     res.json(feedback);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to fetch feedback', error });
+//   }
+// };
 
-    if (
-      typeof chatSessionId !== 'number' ||
-      typeof rating !== 'string' ||
-      typeof content !== 'string'
-    ) {
-      return res.status(400).json({ message: 'Invalid input' });
-    }
+// export const createFeedback = async (req: Request, res: Response) => {
+//   try {
+//     const { chatSessionId, rating, content } = req.body;
 
-    const feedback = await feedbackService.createFeedback(
-      chatSessionId,
-      rating,
-      content,
-    );
-    res.status(201).json(feedback);
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to create chat session', error });
-  }
-};
+//     if (
+//       typeof chatSessionId !== 'number' ||
+//       typeof rating !== 'string' ||
+//       typeof content !== 'string'
+//     ) {
+//       return res.status(400).json({ message: 'Invalid input' });
+//     }
 
-export const deleteFeedback = async (req: Request, res: Response) => {
-  try {
-    const id = parseInt(req.params.id);
-    const success = await feedbackService.deleteFeedback(id);
-    if (!success) {
-      return res.status(404).json({ message: 'Feedback not found' });
-    }
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).json({ message: 'Failed to delete feedback', error });
-  }
-};
+//     const feedback = await feedbackService.createFeedback(
+//       chatSessionId,
+//       rating,
+//       content,
+//     );
+//     res.status(201).json(feedback);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to create chat session', error });
+//   }
+// };
+
+// export const deleteFeedback = async (req: Request, res: Response) => {
+//   try {
+//     const id = parseInt(req.params.id);
+//     const success = await feedbackService.deleteFeedback(id);
+//     if (!success) {
+//       return res.status(404).json({ message: 'Feedback not found' });
+//     }
+//     res.status(204).send();
+//   } catch (error) {
+//     res.status(500).json({ message: 'Failed to delete feedback', error });
+//   }
+// };
