1b7358a7e4808a692467b286b44921618c9b9b2a
August <vuonglocthinh2003@gmail.com>
2025-04-24 13:51:15 +0700

    Add findAll, findByTenantId, findById functions in document repository

--- src/repository/document.repository.ts
+++ src/repository/document.repository.ts
@@ -8,4 +8,20 @@ export class DocumentRepository {
   public async save(document: DocumentEntity): Promise<DocumentEntity> {
     return await this.documentRepository.save(document);
   }
+
+  public async findAll(): Promise<DocumentEntity[]> {
+    return await this.documentRepository.find();
+  }
+
+  public async findByTenantId(tenantId: number): Promise<DocumentEntity[]> {
+    return await this.documentRepository.find({
+      where: { tenantId: tenantId },
+    });
+  }
+
+  public async findById(id: number): Promise<DocumentEntity | null> {
+    return await this.documentRepository.findOne({
+      where: { id: id },
+    });
+  }
 }
