250dd93727d6f8dddcefafdba2d3691d27dd8dde
August <vuonglocthinh2003@gmail.com>
2025-05-06 21:47:20 +0700

    Edit user chatbot migration file

--- src/database/migration/1746262374304-CreateUserChatbot.ts
+++ src/database/migration/1746262374304-CreateUserChatbot.ts
@@ -5,11 +5,10 @@ export class CreateUserChatbot1746262374304 implements MigrationInterface {
     await queryRunner.query(`
             CREATE TABLE USER_CHATBOT (
                 ID BIGINT NOT NULL AUTO_INCREMENT,
-                USER_ID BIGINT NOT NULL UNIQUE,
                 TENANT_ID BIGINT NOT NULL,
                 NAME VARCHAR(255) NOT NULL,
+                PHONE_NUMBER VARCHAR(255) NOT NULL,
                 PRIMARY KEY (ID),
-                FOREIGN KEY (USER_ID) REFERENCES USER(ID),
                 FOREIGN KEY (TENANT_ID) REFERENCES TENANT(ID)
             );
         `);
